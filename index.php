5bed454ee7d6bae10585a062dc5ba4e99702b345
affankhan43 <affankhan627@gmail.com>
2018-04-01 04:37:55 +0500

    'username'

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 include 'core/.env';
-$db = mysqli_connect($HostName,$HostUser,$HostPass,$dbName);
+$db = mysqli_connect($HostName,$HostUser,$HostPass,$dbName) or die("Could not connect to the database");
+if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    else{
+       // echo "sdf";
+    }
 print_r($db);
 session_start();
   require __DIR__ . '/vendor/autoload.php';
