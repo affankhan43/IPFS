4a08ffb1116019622d02e1c19012226c87ec4466
affankhan43 <affankhan627@gmail.com>
2018-04-03 03:33:49 +0500

    'username'

--- index.php
+++ index.php
@@ -227,7 +227,7 @@ session_start();
 <?php } ?>
             <div class="form-group">
               <label for="passport-image">View Document By IPFS HASH</label>
-              <input type="file" name="docx" class="form-control-file uploader" accept=".png, .jpg, .jpeg" required>
+              <input type="text" name="hash_form" class="form-control" required>
               <input type="hidden" name="xss_code" value=<?php echo xss_code_generate(); ?> required>
               <input type="submit" name="upload_now" class="form-control btn btn-info" value="Upload Now!" required>
             </div>
