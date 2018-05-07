896b1119457a8f16187c3bcbab0414542233ac02
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 16:17:25 +0500

    index

--- index.php
+++ index.php
@@ -69,6 +69,9 @@
 			</a>
 			<div class="col-md-1"></div>
 			</div>
+			<br/>
+			<br/>
+			<br/>
 			<div class="row">
 				<div class="col-md-12">
 					<h3 class="text-center">Verify Anything</h3>
