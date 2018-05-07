c280dcccaa973932558677b5ec9e9e2920b26608
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 23:12:23 +0500

    pdf

--- core/pdf.php
+++ core/pdf.php
@@ -6,7 +6,7 @@ require('fpdf/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->MultiCell(0,5,utf8_decode('asdasdasdasd' . chr(10) . 'zxczxczx'),0);
+$pdf->MultiCell(0,5,utf8_decode('asdasdasdasd' . chr(10).chr(10) . 'zxczxczx'),0);
 $pdf->Output();
 
 // if(isset($_POST['action']) && $_POST['action'] == 'savepdf'){
