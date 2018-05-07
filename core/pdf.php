54c443f55be8725233f7c240385ce6fa23820d17
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-08 00:00:11 +0500

    pdf

--- core/pdf.php
+++ core/pdf.php
@@ -2,7 +2,18 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-echo $_POST['formdata'] . '  ' . $_POST['filedata'];
+
+
+
+  // require('fpdf/fpdf.php');
+  // $pdf = new FPDF();
+  // $pdf->AddPage();
+  // $pdf->SetFont('Arial','B',16);
+  // $pdf->MultiCell(0,5,"asdasd \nasdasdsa \nzxczxczxc ",0);
+  // $pdf->Output();
+
+
+}
 
 
  ?>
