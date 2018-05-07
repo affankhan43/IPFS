fb40c3c637ec3b7bd685c6b1451e835ee0c94608
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 17:04:45 +0500

    index

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
 		color: #3d3d3d;
 		background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
 		transition: all 0.7s ease-in;
+		-webkit-box-shadow: 3px 3px 5px 6px #ccc;  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
+		-moz-box-shadow:    3px 3px 5px 6px #ccc;  /* Firefox 3.5 - 3.6 */
+		box-shadow:         3px 3px 5px 6px #ccc;  /* Opera 10.5, IE 9, Firefox 4+, Chrome 6+, iOS 5 */
 	}
 	.eth h3{
 		font-weight: bold;
