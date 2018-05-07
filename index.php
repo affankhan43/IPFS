7a312e3de80656244c25389425778fcefb79a987
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 14:30:35 +0500

    index

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
 				background: #ECE9E6;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+padding: 100px;
 			}
 		</style>
 	</head>
 	<body>
 
-		<div class="jumbotron container">
+		<div class="container">
 			<div class="row">
 				<div class="col-sm-6">
 					<div class="card">
@@ -27,7 +28,14 @@ background: linear-gradient(to right, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, F
 					</div>
 				</div>
 				<div class="col-sm-6">
-
+					<div class="card">
+					  <img class="card-img-top" src="..." alt="Card image cap">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
 				</div>
 			</div>
 		</div>
