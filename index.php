ed56335d3cfbd6e0d7f8a09b09b1069027240031
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 19:04:55 +0500

    index

--- index.php
+++ index.php
@@ -97,6 +97,5 @@ src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLN
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="form-builder.min.js"></script>
 <script type="text/javascript" src="form-render.min.js"></script>
-<script type="text/javascript" src="demo.js"></script>
 </body>
 </html>
