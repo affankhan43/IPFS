3b9d640d40048d5751df4c62ac43c7f3bc3a36bf
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 23:04:07 +0500

    pdf

--- core/pdf.php
+++ core/pdf.php
@@ -3,10 +3,18 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 require('fpdf/fpdf.php');
-
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,20,'Hello World!');
 $pdf->Output();
+
+// if(isset($_POST['action']) && $_POST['action'] == 'savepdf'){
+//
+//
+//
+// }
+
+
  ?>
