f24db916a1b76ed95c84160500902a50ac05a900
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 22:54:35 +0500

    pdf

--- core/pdf.php
+++ core/pdf.php
@@ -3,9 +3,10 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 require('fpdf.php');
+
 $pdf = new FPDF();
 $pdf->AddPage();
-$pdf->SetFond('Arial', 'B', 18)
-$pdf->Cell(40,10, 'This is demo')
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
 $pdf->Output();
  ?>
