3d79ba3e202c89d304a3cd01e1ddc76225a02248
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-07 22:44:12 +0500

    pdf

--- core/pdf.php
+++ core/pdf.php
@@ -4,8 +4,8 @@ ini_set("display_errors", 1);
 require 'fpdf181/fpdf.php';
 
 $pdf = new FPDF();
-$pdf => AddPage();
-$pdf => SetFond('Arial', 'B', 18)
-$pdf => Cell(40,10, 'This is demo')
-$pdf =>output();
+$pdf -> AddPage();
+$pdf -> SetFond('Arial', 'B', 18)
+$pdf -> Cell(40,10, 'This is demo')
+$pdf -> output();
  ?>
