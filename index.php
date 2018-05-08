c65438c4ead1214533d3a03d1c1d560093b2212c
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-08 21:19:52 +0500

    index

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>IPFS</title>
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Gothic+A1" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
 	<style media="screen">
 	body{
 		padding: 80px;
@@ -50,49 +51,122 @@
 	#form-submission{
 		display: none;
 	}
+	/* The sidebar menu */
+	.sidenav {
+		 height: 100%; /* Full-height: remove this if you want "auto" height */
+		 width: 220px; /* Set the width of the sidebar */
+		 position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+		 letter-spacing: .2rem;
+		 padding: .75rem 1rem;
+		 z-index: 1; /* Stay on top */
+		 top: 0; /* Stay at the top */
+		 left: 0;
+		 background-color: #7376df; /* Black */
+		 overflow-x: hidden; /* Disable horizontal scroll */
+		 padding-top: 20px;
+	}
+
+	/* The navigation menu links */
+	.sidenav a {
+		 padding: 6px 8px 6px 16px;
+		 text-decoration: none;
+		 font-size: 15px;
+		 color: #fff !important;
+		 display: block;
+		 cursor: pointer;
+	}
+
+	/* When you mouse over the navigation links, change their color */
+	.sidenav a:hover {
+		 color: #f1f1f1;
+}
+
+/* Style page content */
+.main {
+	 margin-left: 160px; /* Same as the width of the sidebar */
+	 padding: 0px 10px;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+	 .sidenav {padding-top: 15px;}
+	 .sidenav a {font-size: 18px;}
+}
+.sidenav_header{
+	font-size: 20px !important;
+	font-weight: bold;
+	text-align: center;
+	margin-bottom: 10px;
+}
+input{
+	background-color: #ffffff;
+	border: solid 1px #efefef;
+box-shadow: none;
+padding: 10px;
+border-radius: 5px;
+}
+input.form-control{
+	height: inherit !important;
+}
 	</style>
 </head>
 <body>
-	<div id="select-blockchain">
-		<h2 class="text-center" style="font-weight:bold;">Select Blockchain</h2>
-		<br/>
-		<br/>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-1"></div>
-				<div class="col-md-4 eth">
-						<br><br>
-						<img src="bitcoin.png" width="100px" height="100px"><br><br>
-						<h3>Bitcoin Blockchain</h3><br>
-						<button onclick="selectBlock();" type="button" class="btn btn-lg btn-block btn-primary">Select</button>
-						<br>
-				</div>
-				<div class="col-md-2"></div>
-				<div class="col-md-4 eth">
-						<br><br>
-						<img src="eth.svg" width="100px" height="100px"><br><br>
-						<h3>Ethereum Blockchain</h3><br>
-						<button type="button" class="btn btn-lg btn-block btn-primary">Coming Soon</button>
-						<br>
-				</div>
-				<div class="col-md-1"></div>
-				</div>
-				<br/>
-				<br/>
-				<br/>
-				<div class="row">
-					<div class="col-md-12">
-						<h3 class="text-center">Verify Anything</h3>
-					</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="sidenav">
+					<a class="sidenav_header">IPFS</a>
+				  <a onclick="verifPage()">Verfiy</a>
 				</div>
 			</div>
+			<div class="col-sm-9">
+				<div id="select-blockchain">
+					<h2 class="text-center" style="font-weight:bold;">Select Blockchain</h2>
+					<br/>
+					<br/>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-1"></div>
+							<div class="col-md-4 eth">
+									<br><br>
+									<img src="bitcoin.png" width="100px" height="100px"><br><br>
+									<h3>Bitcoin Blockchain</h3><br>
+									<button onclick="selectBlock();" type="button" class="btn btn-lg btn-block btn-primary">Select</button>
+									<br>
+							</div>
+							<div class="col-md-2"></div>
+							<div class="col-md-4 eth">
+									<br><br>
+									<img src="eth.svg" width="100px" height="100px"><br><br>
+									<h3>Ethereum Blockchain</h3><br>
+									<button type="button" class="btn btn-lg btn-block btn-primary">Coming Soon</button>
+									<br>
+							</div>
+							<div class="col-md-1"></div>
+							</div>
+							<br/>
+							<br/>
+							<br/>
+							<div class="row">
+								<div class="col-md-12">
+									<h3 class="text-center">Verify Anything</h3>
+								</div>
+							</div>
+						</div>
+				</div>
+
+				<form id="form-submission">
+					<h3>Submit Form</h3>
+					<div id="form"></div>
+					<button onclick="submitForm();" type="button" name="button" class="btn btn-lg btn-block btn-primary">Submit</button>
+				</form>
+
+			</div>
+		</div>
 	</div>
 
-	<form id="form-submission">
-		<h3>Submit Form</h3>
-		<div id="form"></div>
-		<button onclick="submitForm();" type="button" name="button" class="btn btn-lg btn-block btn-primary">Submit</button>
-	</form>
+
 
 
 <script
@@ -117,6 +191,13 @@ src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLN
 		var fileData = $("form input[type='file']")[0].files[0];
 
 		console.log(formData);
+	}
+
+	function verifPage(){
+		if($("#form-submission").css('display') == 'block'){
+			$("#select-blockchain").fadeIn();
+			$("#form-submission").fadeOut();
+		}
 	}
 
 </script>
