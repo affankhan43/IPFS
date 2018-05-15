435f8b203ed22965e43a0fc27ce54a4c85a4062f
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-16 00:17:20 +0500

    transaction details div

--- index.php
+++ index.php
@@ -187,7 +187,7 @@ input.form-control{
 							<div class="card-header">RECORD IT ON BITCOIN BLOCKCHAIN</div>
 
 							<div class="card-block">
-								<p>Send exactly <div id="resp_fee"></div> </p>
+								<p class="text-center">Send exactly <div id="resp_fee"></div> </p>
 								<img id="resp_qr" src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" />
 								<p id="resp_address"></p>
 							</div>
