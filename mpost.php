b3b1ea0d94041ec319712fea36d1973ed91c9a18
affankhan43 <affankhan627@gmail.com>
2018-05-16 09:56:42 +0500

    'fees'

--- mpost.php
+++ mpost.php
@@ -102,7 +102,7 @@ if(isset($_POST['msg']) && isset($_POST['form_data']) && isset($_POST['fileData'
 							// mail($_POST['email'],"Document Added ...",$mssg,$headers);
 							// $URL = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'].'?hash='.$data_compose['Hash'];
 							// header('Location: '.$URL);
-               				echo json_encode(array("success"=>true,"message"=>"Successfully Data Added",'HASH'=>$data_compose['Hash'], 'address'=>$address['address'], 'fees'=>50000)); 
+               				echo json_encode(array("success"=>true,"message"=>"Successfully Data Added",'HASH'=>$data_compose['Hash'], 'address'=>$address['address'], 'fees'=>$value_fees)); 
 						}
 						else{
 							echo json_encode(array("success"=>false,"message"=>"Unknown Error #11"));
