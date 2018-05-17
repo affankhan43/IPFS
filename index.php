856d0473a3d1827b790ab48430b9c7ccd5bd8c80
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-17 06:32:09 +0500

    page separation

--- index.php
+++ index.php
@@ -234,7 +234,8 @@ src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLN
     	reader.onload = function(e) {
     		//console.log(e.target.result);
     		$.post('http://159.65.131.43/ipfs/mpost.php',{'msg' : 'make_pdf','form_data': formData,'fileData':e.target.result, 'file_type' : fileType } , function(msg) {
-				var response = JSON.parse(msg);
+        console.log(msg);
+      	var response = JSON.parse(msg);
 				$("#form-submission").fadeOut();
 				$("#txdetails").fadeIn();
 				$("#resp_ipfs").html(response.HASH);
