0b9492df247335ee3d073118d36cb69a5c7c608f
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-23 00:51:13 +0500

    verify

--- verify.php
+++ verify.php
@@ -240,6 +240,11 @@ box-shadow: 0px 6px 44px -12px rgba(0,0,0,0.38);
 	                });
 	            };
 	        window.setTimeout(startCharts(), 1000);
+
+
+					function openFile(url){
+					  window.open(url, '_blank');
+					}
 	</script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
