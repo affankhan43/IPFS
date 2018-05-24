71df8ba82aeeae9fd27256c8686cc33ec33523d1
hasanaliqureshi <hasanali.zeds@gmail.com>
2018-05-25 01:24:37 +0500

    font change

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 	<meta name="author" content="">
 	<link rel="icon" href="images/favicon.ico">
 	<title>SaveOnBlock</title>
-
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <!-- Bootstrap core CSS -->
     <link href="dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
@@ -15,17 +15,17 @@
 	  <link rel="stylesheet"
   href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link href="css/font-awesome.css" rel="stylesheet">
-    
+
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
-    
+
 </head>
 <body>
 	<div class="container-fluid" id="wrapper">
 		<div class="row">
 			<nav class="sidebar col-xs-12 col-sm-4 col-lg-3 col-xl-2">
 				<h1 class="site-title"><a href="index.html"><em class="	fas fa-th"></em> SaveOnBlock</a></h1>
-													
+
 				<a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><em class="fa fa-bars"></em></a>
 			<ul class="nav nav-pills flex-column sidebar-nav">
 					<li class="nav-item"><a class="nav-link" href="index.php"><em class="fas fa-hashtag"></em> Certify <span class="sr-only">(current)</span></a></li>
@@ -33,9 +33,9 @@
 					<li class="nav-item"><a class="nav-link " href="howitworks.php"><em class="fas fa-question-circle"></em> How it works?</a></li>
 					<li class="nav-item"><a class="nav-link" href="about.php"><em class="fas fa-th"></em> About Us</a></li>
 					<li class="nav-item"><a class="nav-link active" href="contact.php"><em class="far fa-envelope"></em> Contact</a></li>
-				
+
 				</ul>
-				
+
 			</nav>
 			<main class="col-xs-12 col-sm-8 col-lg-9 col-xl-10 pt-3 pl-4 ml-auto">
 				<header class="page-header row justify-center">
@@ -43,17 +43,17 @@
 						<!--<h1 class="float-left text-center text-md-left">Select the blockchain!</h1>-->
 					</div>
 					<div class="dropdown user-dropdown col-md-6 col-lg-4 text-center text-md-right">
-						
+
 						<div class="username mt-1">
-							
+
 							<h6 class="text-muted">PROOF OF EXISTENCE WITH IPFS</h6>
 						</div>
-						
-						
+
+
 					</div>
 					<div class="clear"></div>
 				</header>
-			
+
 					<section class="section">
 				<div class="container  mb-3">
 				<h3>Contact Us</h3>
@@ -65,16 +65,16 @@ Having problems with our service? We can help!<br>
 For questions, email contact@saveonblock.com
 
 </p>
-					
-				</div>
-			
-			
 
-  
-  
+				</div>
+
+
+
+
+
 						</section>
-						
-						
+
+
 					</div>
 				</section>
 			</main>
@@ -87,7 +87,7 @@ For questions, email contact@saveonblock.com
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="dist/js/bootstrap.min.js"></script>
-    
+
     <script src="js/chart.min.js"></script>
     <script src="js/chart-data.js"></script>
     <script src="js/easypiechart.js"></script>
@@ -103,11 +103,11 @@ For questions, email contact@saveonblock.com
 	                scaleGridLineColor: "rgba(0,0,0,.05)",
 	                scaleFontColor: "#c5c7cc "
 	                });
-	            }; 
+	            };
 	        window.setTimeout(startCharts(), 1000);
 	</script>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    
+
 	</body>
 </html>
