2f56995144fa15ccc037c3c8ade93408e60259aa
affankhan43 <affankhan627@gmail.com>
2018-06-08 00:46:03 +0500

    'table'

--- form.php
+++ form.php
@@ -104,29 +104,19 @@ margin-left: 10px !important;
 								<table class="table table-hover">
 									<thead>
 										<tr>
-											<th>Firstname</th>
-											<th>Lastname</th>
-											<th>Email</th>
+											<th>IPFS Hash</th>
+											<th>Status</th>
+											<th>Bitcoin TXID</th>
 										</tr>
 									</thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
+									<tbody>
+										<tr>
+											<td>John</td>
+											<td>Doe</td>
+											<td>john@example.com</td>
+										</tr>
+									</tbody>
+								</table>
 							</div>
 						</section>
 					</div>
