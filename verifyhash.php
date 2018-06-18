558dcd705921d8c5e8be22f274d3b17d1660c13a
affankhan43 <affankhan627@gmail.com>
2018-06-18 08:54:21 +0500

    'modal'

--- verifyhash.php
+++ verifyhash.php
@@ -312,7 +312,9 @@ session_start();
 		function create_pay(coin){
 			if(coin == 'eth'){
 				$(".modal-title").html(' Pay Ethereum');
-				$(".modal-body").html('loading....').load('https://baconipsum.com/api/?type=meat-and-filler&paras=');
+				$.GET('https://baconipsum.com/api/?type=meat-and-filler&paras',function(msg){
+					$(".modal-body").html(msg);
+				});
 				$("#myModal").modal()
 			}
 			else if(coin == 'btc'){
