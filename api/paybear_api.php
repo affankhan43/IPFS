b065a15d5046f034a29cece2cbf4799a9aa9a293
affankhan43 <affankhan627@gmail.com>
2018-06-20 02:39:02 +0500

    'paybear'

--- api/paybear_api.php
+++ api/paybear_api.php
@@ -15,15 +15,15 @@ header("Access-Control-Allow-Headers: *");
 
 if(isset($_POST['msg'])){
 	if($_POST['msg'] == 'payment_address' && isset($_POST['hash']) && isset($_POST['btc_address']) && isset($_POST['coin'])){
-		$qry = mysqli_query($db,"SELECT `ipfs_hash` FROM `document_details` WHERE ipfs_hash='".$_POST['hash']."' AND bitcoin_address='".$_POST['btc_address']."' ");
+		$qry = mysqli_query($db,"SELECT `ipfs_hash`,`coin_data` FROM `document_details` WHERE ipfs_hash='".$_POST['hash']."' AND bitcoin_address='".$_POST['btc_address']."' ");
 		$result = mysqli_fetch_assoc($qry);
 		if(!$result){
 			echo json_encode(array('success'=>false,'error'=>'hash not found'));
 		}
 		else{
-			$get_url = 'https://api.paybear.io/v2/'.$_POST['coin'].'/payment/?token='.$priv_env_key;
-			$address_data = file_get_contents($get_url);
-			echo json_encode($address_data);
+			//$get_url = 'https://api.paybear.io/v2/'.$_POST['coin'].'/payment/?token='.$priv_env_key;
+			//$address_data = file_get_contents($get_url);
+			echo json_encode($result);
 		}
 	}
 }
