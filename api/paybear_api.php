a1cbf3ad962e586344cd9df6a5898e3734e8e52c
affankhan43 <affankhan627@gmail.com>
2018-06-19 07:18:02 +0500

    'tx'

--- api/paybear_api.php
+++ api/paybear_api.php
@@ -2,9 +2,18 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
+	include '../core/.env';
+	$db = mysqli_connect($HostName,$HostUser,$HostPass,$dbName) or die("Could not connect to the database");
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else{
+		echo "sdf";
+	}
+	require __DIR__ . '/vendor/autoload.php';
 
 if(isset($_POST['msg'])){
-	if($_POST['msg'] == 'payment_address' && isset($_POST[''])){
+	if($_POST['msg'] == 'payment_address' && isset($_POST['hash'])){
 
 	}
 }
