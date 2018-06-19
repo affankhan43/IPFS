7dd94b424ca4cbf19192298d98b9a5857157c74c
affankhan43 <affankhan627@gmail.com>
2018-06-20 02:28:16 +0500

    'paybear'

--- api/paybear_api.php
+++ api/paybear_api.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 	include '../core/.env';
+	include '.env';
 	$db = mysqli_connect($HostName,$HostUser,$HostPass,$dbName) or die("Could not connect to the database");
 	if (mysqli_connect_errno()) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
