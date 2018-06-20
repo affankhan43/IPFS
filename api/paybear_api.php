650046d3680119405e46610fc7a90ae219ed0c5f
affankhan43 <affankhan627@gmail.com>
2018-06-20 05:01:32 +0500

    'paybear'

--- api/paybear_api.php
+++ api/paybear_api.php
@@ -40,10 +40,31 @@ if(isset($_POST['msg'])){
 			}
 			else{
 				$coin_check = json_decode($result['coin_data'],true);
-				for ($i=0; $i < sizeof($coin_check) ; $i++) { 
-					
+				for($i=0; $i < sizeof($coin_check) ; $i++){ 
+					if($coin_check[$i]['coin'] == $_POST['coin']){
+						$coin_exist = $coin_check[$i];
+					}
 				}
-				echo json_encode($result);
+				if(isset($coin_exist)){
+					echo json_encode($coin_exist);
+				}
+				else{
+					$get_url = 'https://api.paybear.io/v2/'.$_POST['coin'].'/payment/?token='.$priv_env_key;
+					$address_data = file_get_contents($get_url);
+					$address_data = json_decode($address_data, true);
+					if($address_data['success'] == true){
+						$coin_data = array(array('coin'=>$_POST['coin'],'address'=>$address_data['data']['address'],'status'=>'pending'));
+						$echo_data = json_encode(array('success'=>true,'coin'=>$_POST['coin'],'address'=>$address_data['data']['address'],'status'=>'pending'));
+						$coin_data = json_encode($coin_data);
+						$upd_qry2 = "UPDATE `document_details` SET `coin_data` = '".$coin_data."'  WHERE ipfs_hash='".$_POST['hash']."' AND bitcoin_address='".$_POST['btc_address']."' ";
+						if(mysqli_query($db, $upd_qry2)){
+							echo $echo_data;
+						}
+					}
+					else{
+						echo json_encode(array('success'=>false,'error'=>'Address Generation Failed'));
+					}
+				}
 			}
 		}
 	}
