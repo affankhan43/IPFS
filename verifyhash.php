00f930675ab6d56ab35564384e2fbd1d7e2f6c1f
affankhan43 <affankhan627@gmail.com>
2018-06-20 06:21:55 +0500

    'tx'

--- verifyhash.php
+++ verifyhash.php
@@ -295,6 +295,9 @@ session_start();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 	<script type="text/javascript">
+		function done(){
+			console.log('asd');
+		}
 	    var startCharts = function () {
 	                var chart1 = document.getElementById("line-chart").getContext("2d");
 	                window.myLine = new Chart(chart1).Line(lineChartData, {
