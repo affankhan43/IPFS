5110669bfe42e92f3a27b923e93040b51a1b0e7d
affankhan43 <affankhan627@gmail.com>
2018-07-07 11:14:09 +0500

    theme

--- form.php
+++ form.php
@@ -112,7 +112,7 @@ margin-left: 10px !important;
 				</div>
 
 
-				<div class="row col-md-12 col-lg-12">
+				<div class="container col-md-12 col-lg-12">
 					<form id="form">
 						
 					</form>
